97df25bd2e4242286fb1363a2d04ffb712635fab
vdr <valeria.do@gmail.com>
2024-02-27 09:18:53 -0600

    created skeleton screen widgets

---
+++ samples/game_template/lib/screens/breath_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:game_template/constants/strings.dart';
+
+class BreathScreen extends StatelessWidget {
+  const BreathScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ samples/game_template/lib/screens/gratitude.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:game_template/constants/strings.dart';
+
+
+class GratitudeScreen extends StatelessWidget {
+  const GratitudeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
