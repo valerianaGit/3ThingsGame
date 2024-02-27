e4dcc143db4bba57a81496474253dddf7a05110b
vdr <valeria.do@gmail.com>
2024-02-27 12:20:31 -0600

    created gratitude screen hierarchy

--- samples/game_template/lib/screens/gratitude_screen.dart
+++ samples/game_template/lib/screens/gratitude_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:game_template/constants/strings.dart';
 
+// Row ->
+// left side is dragon, 
+// right side a Column of 3 gratitude cards -> pregnancy journal
 
 class GratitudeScreen extends StatelessWidget {
   const GratitudeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+body: Row(
+  children: [
+  // dragon asset
+  SizedBox(height: null, width: 80.0,), 
+  // Column 3 gratitude 
+Column(
+  children: [
+    GratitudeCard(),
+    GratitudeCard(),
+    GratitudeCard(),
+  ],
+),
+  ],
+
+),
+//TODO: FLOATING ACTION BUTTON - to store the gratitudes -> pregnancy journal 
+    )
+    ;
   }
 }
