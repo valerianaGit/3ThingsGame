604e978390799cb5892b5e2513670a3902a9485d
vdr <valeria.do@gmail.com>
2024-02-27 12:19:26 -0600

    created gratitude card

---
+++ samples/game_template/lib/widgets/gratitude_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GratitudeCard extends StatelessWidget {
+  const GratitudeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
