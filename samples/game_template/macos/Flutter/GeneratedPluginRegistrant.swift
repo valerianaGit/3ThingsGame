6f62d6a191d3b5e8d96a70c0d3869f5ac7d5b692
vdr <valeria.do@gmail.com>
2024-02-27 23:05:56 -0600

    create drift dependencies

--- samples/game_template/macos/Flutter/GeneratedPluginRegistrant.swift
+++ samples/game_template/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import games_services
 import in_app_purchase_storekit
 import path_provider_foundation
 import shared_preferences_foundation
+import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
@@ -21,4 +22,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
